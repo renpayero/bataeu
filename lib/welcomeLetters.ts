dd70392cf3baa78f37dd4414f2ba97de4df85f56
Renzo <renzo@gmail.com>
2026-05-01 02:29:16 -0300

    modificando cartas

--- lib/welcomeLetters.ts
+++ lib/welcomeLetters.ts
@@ -18,6 +18,8 @@ export const WELCOME_NOSOTROS: WelcomeLetterContent = {
   heading: 'Para vos, mi vida',
   body: `Hace un año que estás conmigo y todavía no me cae la ficha de que sea verdad. Cada mañana que me despierto al lado tuyo me acuerdo de la suerte que tengo.
 
+Sé que no soy bueno para escribirte cartas, pero dentro de esta aplicación vas a encontrar muchas cartitas. Y detrás de todo esto hay más de diez mil líneas de código que escribí para vos 🤍.
+
 Vos sabés que lo mío no son las palabras lindas, pero quería que tuvieras algo que sea solo nuestro. Un rinconcito al que entres y te acuerdes de todo lo que somos.
 
 Gracias por aguantarme. Por reírte conmigo, por bancarme los días raros, por elegirme cada mañana de nuevo.
